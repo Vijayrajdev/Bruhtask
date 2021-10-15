f112432426b7a614e29d00222015df81a38117cc
Vijayraj-wd <vijayrajck@outlook.com>
2021-10-15 22:50:05 +0530

    Frontend design

---
+++ app.js
@@ -0,0 +1,9 @@
+const taskInput = document.getElementById('tasks')
+const addButton = document.querySelector('.addbutton')
+
+addButton.addEventListener('click', () => {
+    console.log(taskInput.value);
+})
+
+
+
