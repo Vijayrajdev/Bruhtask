b88c938379074ab8e0668b4070af70e76ed5ee31
Vijayraj-wd <vijayrajck@outlook.com>
2021-10-16 14:20:59 +0530

    FUNCTION: Deleting task & Editing Task

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 // Selectors
 const taskInput = document.getElementById('tasks')
 const addButton = document.querySelector('.addbutton')
-const completedButton = document.querySelector('.completedbutton')
-const editButton = document.querySelector('.editbutton')
-const deleteButton = document.querySelector('.deletebutton')
-const taskList = document.querySelector('.list__task')
-
+const taskList = document.querySelector('#list__task')
+const completedTaskList = document.querySelector('#list__completed')
+const countSpace = document.querySelector('.Count-space')
 
 // Eventlisteners
-addButton.addEventListener('click', () => addTodo())
+addButton.addEventListener('click', addTodo)
+taskList.addEventListener('click', deleteTask)
+
 
 // Functions
-const addTodo = () => {
-    let element = document.createElement('div')
-    // Tasks Adder template
-    let taskTemplate = `<div
+
+// Adding Task
+function addTodo() {
+
+  // For tasks
+  let element1 = document.createElement('div')
+  element1.classList.add('.task-list')
+
+  // Tasks Adder template
+  let taskTemplate = `<div
       class="task__lists d-flex align-items-center justify-content-around mb-3"
     >
       <div
@@ -23,18 +29,53 @@ const addTodo = () => {
       >
         <p class="list-group-item form-check">${taskInput.value}</p>
         <button type="button" class="btn completedbutton">
-          <ion-icon name="checkmark-done-outline"></ion-icon>
+          <ion-icon name="checkmark-done-outline" class="complete-button"></ion-icon>
         </button>
       </div>
       <div class="d-flex">
         <button type="button" class="btn btn-outline-success editbutton mr-1">
-          <ion-icon name="create-outline"></ion-icon>
+          <ion-icon name="create-outline" class="edit-button"></ion-icon>
         </button>
         <button type="button" class="btn btn-outline-danger deletebutton">
-          <ion-icon name="trash-outline"></ion-icon>
+        <ion-icon name="trash-outline" class="delete-button"></ion-icon>
         </button>
       </div>
     </div>`
-    element.innerHTML = taskTemplate
-    taskList.appendChild(element)
+
+  // For tasks
+  if (!taskInput.value) { console.log("error"); }
+  else {
+    element1.innerHTML = taskTemplate
+    taskList.appendChild(element1)
+  }
+  // Clearing input
+  taskInput.value = ""
+
+}
+
+// Deleting Task
+function deleteTask(e) {
+  const item = e.target
+  if (item.classList[0] === "delete-button") {
+    const todo = item.parentElement.parentElement.parentElement.parentElement
+    todo.remove()
+  } else if (item.classList[2] === 'deletebutton') {
+    const todo = item.parentElement.parentElement.parentElement
+    todo.remove()
+  }
+
+  if (item.classList[0] === "complete-button") {
+    const todo = item.parentElement.parentElement
+    todo.classList.toggle('completed')
+  }
+
+  if (item.classList[0] === 'edit-button') {
+    const todo = item.parentElement.parentElement.parentElement.parentElement
+    todo.setAttribute('contenteditable', "true")
+  }
+
+  if (item.classList[2] === 'addbutton') {
+    const todo = item.parentElement.parentElement.parentElement.parentElement
+    todo.setAttribute('contenteditable', "false")
+  }
 }
